860c3e51952970c76e5a074bb0e04e0fe3d0ff26
Oleksii Khatuntsev <39759290+OleksiiKhatuntsev@users.noreply.github.com>
2019-05-15 11:20:25 +0300

    add a becometrainer page

---
+++ Kursach/Controllers/TrainerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BOL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Web.Controllers
+{
+    [Authorize]
+    public class TrainerController : BaseController 
+    {
+        public TrainerController(CyberTrainingContext context) : base(context)
+        { }
+
+        public IActionResult Index()
+        {
+            var currentRole = db.RoleDb.GetAll().FirstOrDefault(x =>
+                x.RoleId == db.UserDb.GetAll().FirstOrDefault(y => y.UserName == User.Identity.Name).RoleId).RoleName;
+
+            if (currentRole == "Trainer")
+            {
+                return View("MyPlayers");
+            }
+
+            return View("BecomeATrainer");
+        }
+
+
+    }
+}
